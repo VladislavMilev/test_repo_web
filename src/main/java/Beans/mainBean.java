535f9302922f271297ee497d6e910a5923651f1a
Vladislav Milev <jimroot555@gmail.com>
2018-06-25 18:09:25 +0300

    work with form login

--- src/main/java/Beans/mainBean.java
+++ src/main/java/Beans/mainBean.java
@@ -1,37 +1,40 @@
 package Beans;
 
-import org.primefaces.context.PrimeFacesContext;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import java.util.Objects;
 
 @ManagedBean(name = "mainBean")
 @SessionScoped
 public class mainBean {
 
-private String name;
-private int age;
+private String email;
+private String password;
 
 public void message(){
     FacesContext context = FacesContext.getCurrentInstance();
-    context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Title", "Hello " + name));
+    if(Objects.equals(email, "vlad_milev1@mail.ru") && Objects.equals(password, "dofemu37")){
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Success", "Hello admin"));
+    }else {
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Wrong", "Wrong email or password"));
+    }
 }
 
-    public String getName() {
-        return name;
+    public String getEmail() {
+        return email;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public int getAge() {
-        return age;
+    public String getPassword() {
+        return password;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
